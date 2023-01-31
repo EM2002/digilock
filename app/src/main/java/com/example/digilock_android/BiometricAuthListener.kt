57d10861184e97fc5bfd02909d9f46593b83ed0a
EM2002 <60019525+em2002@users.noreply.github.com>
2023-01-31 13:44:01 +0100

    errors everywhere

---
+++ app/src/main/java/com/example/digilock_android/BiometricAuthListener.kt
@@ -0,0 +1,14 @@
+package com.example.digilock_android
+
+import androidx.biometric.BiometricPrompt
+
+class BiometricAuthListener {
+    fun onBiometricAuthenticationError(errorCode: Int, toString: String) {
+
+    }
+
+    fun onBiometricAuthenticationSuccess(result: BiometricPrompt.AuthenticationResult) {
+
+    }
+
+}
--- app/src/main/java/com/example/digilock_android/MainActivity.kt
+++ app/src/main/java/com/example/digilock_android/MainActivity.kt
@@ -1,43 +1,86 @@
 package com.example.digilock_android
 
+import android.annotation.SuppressLint
+import androidx.biometric.BiometricPrompt;
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.util.Log
+import android.view.View
+import android.widget.RadioButton
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.digilock_android.ui.theme.DigilockandroidTheme
+import com.google.android.material.internal.ContextUtils.getActivity
+import java.util.concurrent.Executor
 
 class MainActivity : ComponentActivity() {
+    private lateinit var logView: TextView
+    private var logLineCounter = 0
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DigilockandroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.home)
+        logView = findViewById(R.id.textView2)
+        logView.movementMethod = ScrollingMovementMethod()
+    }
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.balanced ->
+                    if (checked) {
+                        logLineCounter = logText(logView, "Radio balanced pressed successfully\n", "radio")
+                    }
+                R.id.low_power ->
+                    if (checked) {
+                        logLineCounter = logText(logView, "Radio power pressed successfully\n", "radio")
+                    }
+                R.id.tx_high ->
+                    if (checked) {
+                        logLineCounter = logText(logView, "Radio tx_high pressed successfully\n", "radio")
+                    }
+                R.id.tx_medium ->
+                    if (checked) {
+                        logLineCounter = logText(logView, "Radio tx_medium pressed successfully\n", "radio")
+                    }
+                R.id.tx_low ->
+                    if (checked) {
+                        logLineCounter = logText(logView, "Radio tx_low pressed successfully\n", "radio")
+                    }
+                R.id.tx_ultra_low ->
+                    if (checked) {
+                        logLineCounter = logText(logView, "Radio tx_ultra_low pressed successfully\n", "radio")
+                    }
             }
         }
     }
-}
+    fun onClickBiometrics(view: View) {
+        BiometricAuth.showBiometricPrompt(
+            activity = getActivity(applicationContext),
+            listener = this,
+            cryptoObject = null,
+            allowDeviceCredential = true
+        )
+    }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    fun onBiometricAuthenticationError(errorCode: Int, toString: String) {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DigilockandroidTheme {
-        Greeting("Android")
+    }
+
+    fun onBiometricAuthenticationSuccess(result: BiometricPrompt.AuthenticationResult) {
+
     }
 }
+
+@SuppressLint("SetTextI18n")
+fun logText(logView: TextView, text: String, tag: String, line: Int = 0): Int {
+    Log.d(tag, text)
+    logView.text = line.toString() + ": " + text + logView.text
+    return line + 1
+}
+
